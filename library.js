0dc4b8746298e4694c7211fec4e88dd47f9eb487
Henok <henatan99@gmail.com>
2021-03-02 20:23:51 +0300

    adds content in library.js file: adds toggle function for add-book btn; adds constructor function Book for book object; adds addBookToLibrary function to store books in a simple array of myLibrary; adds a simple DOM to create and append a row of book object in the books list table

--- library.js
+++ library.js
@@ -6,3 +6,48 @@ function toggle() {
             x.style.display = "none";
         }
 }
+
+let myLibrary = [];
+
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+// DOM manipulation 
+
+// function listBooks(myLibrary) {
+    const books = document.querySelector('#books');
+    // myLibrary.forEach
+    
+        // creating nodes 
+        const book = document.createElement('tr');
+        book.classList.add('book');
+
+        // rendering node 'tr'
+        books.appendChild(book);
+
+        // creating nodes 'td'
+        const title = document.createElement('td');
+        title.classList.add('book-item');
+        title.textContent = 'Book Title';
+
+        const author = document.createElement('td');
+        author.classList.add('book-item');
+        author.textContent = 'Book Author';
+
+        const isbn = document.createElement('td');
+        isbn.classList.add('book-item');
+        isbn.textContent = 'Book isbn';
+
+        // rendering nodes 'td'
+
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(isbn);     
+// }
