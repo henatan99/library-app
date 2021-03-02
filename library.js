721bb70eb5bbcdef4ab643a8541122c4b6d392df
Henok <henatan99@gmail.com>
2021-03-02 23:07:03 +0300

    adds appendBook function to create tr elements for the books list row and append it to the parent node, create td elements for the books attributes and append to the parent tr node; adds listBooks function to call appendBook function on the book object to be rendered; calls listBooks function upon myLibrary books collection

--- library.js
+++ library.js
@@ -7,7 +7,12 @@ function toggle() {
         }
 }
 
+// Book list table 
+
+// array variable to hold book objects 
 let myLibrary = [];
+
+// constructor function for Book object
 
 function Book(title, author, isbn) {
   this.title = title;
@@ -15,39 +20,58 @@ function Book(title, author, isbn) {
   this.isbn = isbn;
 }
 
+// adding book to myLibrary collection
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
 // DOM manipulation 
 
-// function listBooks(myLibrary) {
-    const books = document.querySelector('#books');
-    // myLibrary.forEach
+function appendBook(bookObj, booksId) {
+    const books = document.querySelector(booksId);
     
         // creating nodes 
-        const book = document.createElement('tr');
-        book.classList.add('book');
+    const book = document.createElement('tr');
+    book.classList.add('book');
 
         // rendering node 'tr'
-        books.appendChild(book);
+    books.appendChild(book);
 
         // creating nodes 'td'
-        const title = document.createElement('td');
-        title.classList.add('book-item');
-        title.textContent = 'Book Title';
+    const title = document.createElement('td');
+    title.classList.add('book-item');
+    title.textContent = bookObj.title;
 
-        const author = document.createElement('td');
-        author.classList.add('book-item');
-        author.textContent = 'Book Author';
+    const author = document.createElement('td');
+    author.classList.add('book-item');
+    author.textContent = bookObj.author;
 
-        const isbn = document.createElement('td');
-        isbn.classList.add('book-item');
-        isbn.textContent = 'Book isbn';
+    const isbn = document.createElement('td');
+    isbn.classList.add('book-item');
+    isbn.textContent = bookObj.isbn;
 
         // rendering nodes 'td'
 
-        book.appendChild(title);
-        book.appendChild(author);
-        book.appendChild(isbn);     
-// }
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(isbn);     
+}
+
+// create book objects manually 
+
+const book1 = new Book('Book1', 'Author1', 'isbn1');
+addBookToLibrary(book1);
+const book2 = new Book('Book2', 'Author1', 'isbn1');
+addBookToLibrary(book2);
+const book3 = new Book('Book3', 'Author1', 'isbn1');
+addBookToLibrary(book3);
+
+// function to render book table list rows 
+
+function listBooks(books) {
+    books.forEach((bookObj) => appendBook(bookObj, '#books'));
+}
+
+// calling listBooks function upon the myLibrary books collection 
+
+listBooks(myLibrary);
